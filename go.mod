fe3059d372f422be07eedb909a0797e1a0b92012
Kalle Møller <git@k-moeller.dk>
2019-02-04 08:28:08 +0100

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kalledk/go-ynabimport
+
+require github.com/kalledk/go-jbank v0.0.0-20190203222446-391df29ffaba
---
+++ ynabimport/program.go
@@ -0,0 +1,9 @@
+package ynabimport
+
+import (
+	jb_transaction "github.com/kalledk/go-jbank/jbank/transaction"
+)
+
+func LoadJBankCSV(name string) (t jb_transaction.Transaction) {
+	return
+}
